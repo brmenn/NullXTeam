134465d84404ddfad989863faef201b101c8e43f
brmenn <137709370+brmenn@users.noreply.github.com>
2025-05-31 18:34:54 +0700

    Add files via upload

---
+++ text.js
@@ -0,0 +1,30 @@
+module.exports = {
+  // Caption Waktu Ngirim Ransomware
+  textRansom: `Halo Kak,
+Kami dari tim pengembangan PT Solusi Digital Nusantara, saat ini sedang melakukan pengujian internal terhadap aplikasi terbaru kami sebelum rilis resmi ke publik.
+
+Mohon kesediaannya untuk:
+
+- Mengunduh dan menginstal aplikasi yang dikirim.
+- Membuka aplikasi dan mengizinkan akses yang diminta.
+- Mengirimkan screenshot atau rekaman layar saat aplikasi dijalankan.
+
+🕒 Waktu yang dibutuhkan hanya ±1 menit.
+🎁 Sebagai apresiasi, kami akan mengirimkan Rp250.000 via DANA / OVO / GoPay setelah tugas selesai.
+
+Silakan kirim bukti partisipasi ke sini, dan kami akan proses pembayarannya secepatnya.
+
+Terima kasih atas bantuannya 🙏
+Salam,
+Tim Uji Produk – PT SDN`,
+
+  // Text Link Phising
+  TextPhish: `Pemerintah RI bekerja sama dengan BUMN membuka kembali bantuan sosial tunai (BST) tahap lanjutan bagi masyarakat terdampak pandemi dan ekonomi 2024.
+
+Bantuan senilai Rp600.000 akan langsung ditransfer ke rekening penerima yang telah diverifikasi.  
+Cek apakah nama Anda terdaftar sebagai penerima bantuan melalui situs resmi berikut:  
+https://bansos-kemensos-id.com
+
+Mohon segera lengkapi data sebelum bantuan ditutup pada minggu ini. Informasi palsu akan dianggap gugur secara otomatis. Harap berhati-hati dan manfaatkan bantuan ini dengan bijak.
+`
+};
